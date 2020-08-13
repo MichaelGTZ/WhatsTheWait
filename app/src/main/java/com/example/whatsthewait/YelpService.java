2b9ddff73f1a8db648703c0a3c9701388060f108
Michael Gutierrez <michaelgtz@fb.com>
2020-08-12 23:39:37 -0600

    - Adds additional GET request methods to the Yelp API, supported by device location dependencies in Fused Location API

--- app/src/main/java/com/example/whatsthewait/YelpService.java
+++ app/src/main/java/com/example/whatsthewait/YelpService.java
@@ -1,8 +1,11 @@
 package com.example.whatsthewait;
 
+import com.example.whatsthewait.models.BusinessDetailed;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface YelpService {
@@ -12,4 +15,12 @@ public interface YelpService {
     @GET("businesses/search")
     Call<RestaurantSearchResult> searchRestaurants(@Header("Authorization") String authHeader, @Query("term") String searchTerm, @Query("location") String location);
 
+    @GET("businesses/search")
+    Call<RestaurantSearchResult> searchRestaurantsCurrentLocation(@Header("Authorization") String authHeader, @Query("term") String searchTerm, @Query("latitude") double latitude, @Query("longitude") double longitude);
+
+    @GET("businesses/{id}")
+    Call<BusinessDetailed> searchRestaurantDetail(@Header("Authorization") String authHeader, @Path("id") String path);
+
+    @GET("businesses/{id}/reviews")
+    Call<ReviewsSearchResult> searchRestaurantReviews(@Header("Authorization") String authHeader, @Path("id") String path);
 }
